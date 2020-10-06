633ef9fe5bc23c22e9b1db0e4544d1f87067d639
Ante Primorac <anteprimorachr@gmail.com>
2020-10-06 22:14:58 +0200

    chore: add split-techcity pm2 app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,25 @@
 module.exports = {
   apps: [
     {
-      name: 'handbook-outline',
-      script: 'index.js',
+      name: 'agilo-handbook',
+      script: 'build/server/index.js',
       watch: '.',
       env: {
         NODE_ENV: 'production',
+        URL: 'https://handbook.agilo.co',
+        PORT: 3000,
+        GOOGLE_ALLOWED_DOMAINS: 'agilo.co',
+      },
+    },
+    {
+      name: 'split-techcity-handbook',
+      script: 'build/server/index.js',
+      watch: '.',
+      env: {
+        NODE_ENV: 'production',
+        URL: 'https://handbook.split-techcity.com',
+        PORT: 4000,
+        GOOGLE_ALLOWED_DOMAINS: 'split-techcity.com',
       },
     },
   ],
